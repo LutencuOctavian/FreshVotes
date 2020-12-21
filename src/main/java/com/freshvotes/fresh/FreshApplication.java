01286af0718592e32887a11b77e5533681f397b1
Lutencu Octavian <octavv2001@yahoo.com>
2020-12-21 18:02:51 +0200

    Initial commit

---
+++ src/main/java/com/freshvotes/fresh/FreshApplication.java
@@ -0,0 +1,13 @@
+package com.freshvotes.fresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/freshvotes/fresh/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.freshvotes.fresh;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FreshApplication.class);
+	}
+
+}
